b03e80ca6fb0ea503756b64abf710445957c95a2
rnoh96 <60715552+rnoh96@users.noreply.github.com>
2022-02-22 22:39:11 -0800

    cart item validator
    checks if an item is already in the cart. If an item exists in the cart it doesn't get added.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Event listener that adds item to the cart
 let addToCartBtn = document.querySelectorAll(".addToCartBtn");
 let cartList = document.querySelector(".cart-list");
 
@@ -7,46 +8,88 @@ addToCartBtn.forEach(btn => {
         let itemName = item[0].innerText
         let imgSrc = item[1].querySelector("img").src
         let itemPrice = item[2].innerText.split("\n")[0]
-        
-        addItem(imgSrc, itemName)
-        addPrice(itemPrice)
-        addQuantity()
 
+        let cartItemNodes = document.querySelectorAll(".cart-item")
+
+// FIXME -  Lots of improvements can be made here ################################
+        let validator = 1
+        if(cartItemNodes.length != 0) {
+            cartItemNodes.forEach(item => {
+                let name = item.children[0].innerText
+                name = name.replace('"', '')
+
+                if(name == itemName){
+                    alert(`${name} already exists in the cart`);
+                    validator = 0
+                }
+            })
+        }
+
+        if(validator == 1) {
+            let div = document.createElement("div")
+            div.className = "cart-item"
+            cartList.append(div)
+
+            cartItemNodes = document.querySelectorAll(".cart-item")
+
+            let cartItem = cartItemNodes[cartItemNodes.length - 1]
+
+            addItem(imgSrc, itemName, cartItem)
+            addPrice(itemPrice, cartItem)
+            addQuantity(cartItem)
+        }
     })
 })
 
 // Function that adds item image and the name to the cart list
-let addItem = (imgSrc, name) => {
+let addItem = (imgSrc, name, element=undefined) => {
     let img = document.createElement("img");
     let div = document.createElement("div")
 
     img.src = imgSrc
-    let item = div
-    item.append(img)
-    item.append(name)
 
-    cartList.append(item)
+    div.append(img)
+    div.append(name)
+
+    div.className = "cart-item-div"
+
+    if(element) {
+        element.append(div)
+    }
 }
 
 // Function that adds the price to the cart list
-let addPrice = (price) => {
+let addPrice = (price, element=undefined) => {
     let div = document.createElement("div")
 
     div.append(price)
 
-    cartList.append(div)
+    div.className = "cart-item-div"
+
+    if(element) {
+        element.append(div)
+    }
 }
 
 // Function that adds quantity of the item and remove button
-let addQuantity = () => {
+let addQuantity = (element=undefined) => {
     let div = document.createElement("div")
     let input = document.createElement("input")
     let btn = document.createElement("button")
 
     btn.textContent = "Remove"
+    btn.className = "remove-btn"
+    input.type = "number"
+    input.value = 1
 
     div.append(input)
     div.append(btn)
 
-    cartList.append(div)
+    div.className = "cart-item-div"
+
+    if(element) {
+        element.append(div)
+    }
 }
+
+// Event listener that removes item in the cart
